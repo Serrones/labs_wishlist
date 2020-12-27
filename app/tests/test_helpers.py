dde88182be0da8ee44cdccf4cd6bbc1025b3476d
serrones <fabioserrones@gmail.com>
2020-12-27 18:45:52 -0300

    Improve helpers and models tests

---
+++ app/tests/test_helpers.py
@@ -0,0 +1,29 @@
+from app.helpers import get_user_by_username
+from app.views import create_user
+
+
+class TestGetUserByUsername:
+
+    def test_should_return_user(
+        self,
+        app,
+        user_payload
+    ):
+        with app.test_request_context(
+            '/users', json=user_payload
+        ):
+            create_user()
+
+            user = get_user_by_username(user_payload['username'])
+
+            assert user.username == user_payload['username']
+            assert user.email == user_payload['email']
+            assert user.is_admin == user_payload['is_admin']
+
+    def test_should_return_none_when_user_not_found(
+        self,
+        app
+    ):
+        user = get_user_by_username('fake_username')
+
+        assert user == None
--- app/tests/test_models.py
+++ app/tests/test_models.py
@@ -14,27 +14,37 @@ class TestUser:
         user.set_password('fake_password')
         return user
 
-    def test_verify_user_repr(
-        self,
-        user
-    ):
+    def test_verify_user_repr(self, user):
         assert user.__repr__() == '<User fake>'
 
-    def test_verify_password(
-        self,
-        user
-    ):
+    def test_verify_user_as_dict(self, user):
+
+        user_dict = {
+            'id': user.id,
+            'username': user.username,
+            'email': user.email,
+            'is_admin': user.is_admin,
+            'products': [product.as_dict() for product in user.products]
+        }
+        assert user.as_dict() == user_dict
+
+    def test_verify_user_from_dict(self):
+
+        user_dict = {
+            'username': 'fake_user',
+            'email': 'fake_user@email',
+            'is_admin': False
+        }
+        user = User()
+        user.from_dict(user_dict)
+        
+        assert user.username == user_dict['username']
+        assert user.email == user_dict['email']
+        assert user.is_admin == user_dict['is_admin']
+
+    def test_verify_password(self, user):
         assert user.check_password('fake_password')
         assert not user.check_password('just_fake')
-
-    def test_as_dict_should_return_username_and_email(
-        self,
-        user
-    ):
-        user_dict = user.as_dict()
-
-        assert user_dict['username'] == 'fake'
-        assert user_dict['email'] == 'fake@email.com'
 
 
 class TestProduct:
@@ -45,8 +55,17 @@ class TestProduct:
             product_id='fake_product_id'
         )
 
-    def test_verify_product_repr(
-        self,
-        product
-    ):
+    def test_verify_product_repr(self, product):
         assert product.__repr__() == '<Product fake_product_id>'
+
+    def test_verify_product_as_dict(self, product):
+
+        product_dict = {
+            'id': product.id,
+            'product_id': product.product_id,
+            'price': product.price,
+            'image': product.image,
+            'brand': product.brand,
+            'title': product.title
+        }
+        assert product.as_dict() == product_dict
